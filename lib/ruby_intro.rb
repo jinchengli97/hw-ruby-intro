aa2d3cf0d68302f364a4c3987c871d7b317c82fe
EC2 Default User <ec2-user@ip-172-31-2-10.us-east-2.compute.internal>
2021-02-09 03:57:46 +0000

    1st

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,96 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  elsif arr.length() == 1
+    return arr[0]
+  else
+    arr.sort!
+    return arr[-1]+arr[-2]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arrlen = arr.length()
+  if arrlen < 2
+    return false
+  end
+  for a in 0..(arrlen-2) do
+    for b in (a+1)..(arrlen-1) do
+      if arr[a]+arr[b] == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  str = "Hello, " + name
+  return str
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  end
+  if s[0].match(/[a-zA-Z]/)
+    if s[0].match(/[aeiouAEIOU]/)
+      return false
+    end
+    return true
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  end
+  if s !~ /[^01]/
+    temp = s.to_i(2)
+    if temp % 4 == 0
+      return true
+    end
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.empty? or price <= 0
+      raise ArgumentError
+    else
+      @BIS_isbn, @BIS_price = isbn, price
+    end
+  end
+  
+  def price_as_string
+    result = "$%0.2f" % [@BIS_price]
+  end
+  
+  #getter
+  def price
+    @BIS_price
+  end
+  def isbn
+    @BIS_isbn
+  end
+  
+  #setter
+  def price=(price)
+    @BIS_price = price
+  end
+  def isbn=(isbn)
+    @BIS_isbn = isbn
+  end
 end
